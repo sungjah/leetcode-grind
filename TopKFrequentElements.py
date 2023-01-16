ebb8a904e4d5bb9df1e29520d492092075571986
sungjah <sungjaehong10@gmail.com>
2023-01-16 01:58:48 -0800

    grind 24

--- TopKFrequentElements.py
+++ TopKFrequentElements.py
@@ -8,3 +8,22 @@ class Solution:
                 count[ord(c) - ord("a")] += 1
             ans[tuple(count)].append(s)
         return ans.values()
+
+
+class Solution2:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Bucket Sort
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
